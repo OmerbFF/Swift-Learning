4aebda3886d2e82d5b5aaca50dc04b89daef2d88
OmerFF <omerrbirerr@gmail.com>
2022-03-05 16:50:06 +0300

    Swift Learning

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Ömer Birer on 5.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        
+        imageView.image = UIImage(named: "mete")
+        
+    }
+    
+}
+
